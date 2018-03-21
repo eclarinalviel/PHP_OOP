1cda70acae1303e12e77e93740048788c30fc61e
Viel Eclarinal <eclarinalviel@gmail.com>
2018-03-21 13:32:58 +0800

    First try

---
+++ dbcrud.php
@@ -0,0 +1,22 @@
+<?php include 'connection.php';
+class Connect
+{
+	public function connect()
+	{
+		mysql_connect("localhost","root");
+		mysql_select_db("practice");
+	}
+	public function setdata($sql)
+	{
+		mysql_query($sql);
+	}
+	public function getdata($sql)
+	{
+		return mysql_query($sql);
+	}
+		public function delete($sql)
+	{
+	mysql_query($sql);
+	}
+}
+?>
